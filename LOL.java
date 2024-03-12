0cbfb24d8222af5274bd2b985b5425b537c2a393
gulman70 <gulfamchanna70@gmail.com>
2024-03-12 04:11:09 -0700

    3rd commit

--- LOL.java
+++ LOL.java
@@ -1,6 +1,9 @@
 class LOL {
 	public static void main(String args[]) {
 		System.out.println("working");
+		System.out.println("working");
+		System.out.println("working");
+		System.out.println("working");
 
 	}
 
